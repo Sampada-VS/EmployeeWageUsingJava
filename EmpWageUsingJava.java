5caa67b2ab001e9d340f1efa2264a5e84dbebbda
Sampada-VS <samssvv1@gmail.com>
2020-08-15 02:04:24 +0530

    Adding monthly wage feature

--- EmpWageUsingJava.java
+++ EmpWageUsingJava.java
@@ -3,25 +3,36 @@ public class EmpWageUsingJava
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
 
 	public static void main(String[] args)
 	{
+		calculate20Wage();
+	}
+	public static void calculate20Wage()
+	{
 		int empWage;
 		int empHrs;
+		int totalEmpWage=0;
 
-		int empCheck=(int)((Math.random()*10)%3);
-		switch(empCheck)
+		for(int day=1; day<=NUM_OF_WORKING_DAYS; day++)
 		{
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
+			int empCheck=(int)((Math.random()*10)%3);
+			switch(empCheck)
+			{
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+			}
+			empWage=empHrs*RATE_PER_HOUR;
+			System.out.println("Employee daily wage: "+empWage);
+			totalEmpWage=totalEmpWage+empWage;
 		}
-		empWage=empHrs*RATE_PER_HOUR;
-		System.out.println("Employee daily wage: "+empWage);
+		System.out.println("Total Employee Wage : "+totalEmpWage);
 	}
 }
