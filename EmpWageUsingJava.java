4672236666f8fab5036c11aaa806e67f46dd9e9c
Sampada-VS <samssvv1@gmail.com>
2020-08-16 23:28:20 +0530

    Adding feature for calculating wage till working hours reached

--- EmpWageUsingJava.java
+++ EmpWageUsingJava.java
@@ -4,6 +4,7 @@ public class EmpWageUsingJava
 	public static final int IS_PART_TIME=2;
 	public static final int RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HOURS=100;
 
 	public static void main(String[] args)
 	{
@@ -14,9 +15,12 @@ public class EmpWageUsingJava
 		int empWage;
 		int empHrs;
 		int totalEmpWage=0;
+		int totalEmpHours=0;
+		int totalWorkingDays=0;
 
-		for(int day=1; day<=NUM_OF_WORKING_DAYS; day++)
+		while (totalEmpHours<MAX_HOURS && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;
 			int empCheck=(int)((Math.random()*10)%3);
 			switch(empCheck)
 			{
@@ -29,10 +33,22 @@ public class EmpWageUsingJava
 				default:
 					empHrs=0;
 			}
-			empWage=empHrs*RATE_PER_HOUR;
-			System.out.println("Employee daily wage: "+empWage);
-			totalEmpWage=totalEmpWage+empWage;
+			totalEmpHours=totalEmpHours+empHrs;
+			if(totalEmpHours>MAX_HOURS)
+			{
+				totalEmpHours=totalEmpHours-empHrs;
+				totalWorkingDays--;
+				break;
+			}
+			else
+			{
+				empWage=empHrs*RATE_PER_HOUR;
+				System.out.println("On day "+totalWorkingDays+" == Employee Daily wage: "+empWage);
+			}
 		}
+		totalEmpWage=totalEmpHours*RATE_PER_HOUR;
 		System.out.println("Total Employee Wage : "+totalEmpWage);
+		System.out.println("Total Employee Hours : "+totalEmpHours);
+		System.out.println("Total working days : "+totalWorkingDays);
 	}
 }
